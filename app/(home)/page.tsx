05f3a0e39f985b175ae3571db53ee0a60703e55f
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-30 20:42:34 +0000

    :sparkles: feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from 'date-fns'
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <div className="px-5 py-6">
+        <h2 className="text-xl font-bold" >Olá, João Vitor</h2>
+        <p className="capitalize text-sm" > {format(new Date(), "EEEE',' dd 'de' MMMM", {
+          locale: ptBR,
+        }
+        )} </p>
+      </div>
     </>
   );
 }
