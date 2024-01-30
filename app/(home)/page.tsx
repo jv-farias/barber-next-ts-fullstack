a5b96b03562eac1cd94b6c09b4a5c339a1bd0318
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2024-01-30 21:11:18 +0000

    :sparkles: feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from 'date-fns'
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
+      <div className="px-5 pt-6">
         <h2 className="text-xl font-bold" >Olá, João Vitor</h2>
         <p className="capitalize text-sm" > {format(new Date(), "EEEE',' dd 'de' MMMM", {
           locale: ptBR,
         }
-        )} </p>
+        )}</p>
+      </div>
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </>
   );
