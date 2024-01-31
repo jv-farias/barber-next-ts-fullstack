9814ec986c606672855ef108755a83a7d27b90c0
João Vitor Farias <jvmnc232@gmail.com>
2024-01-31 11:17:48 -0300

    :sparkles: feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -36,6 +36,14 @@ export default async function Home() {
           ))}
         </div>
       </div>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&:: -webkit-scrollbar]:hidden " >
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
