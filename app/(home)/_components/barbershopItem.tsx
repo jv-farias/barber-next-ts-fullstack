8a8bef994a8c23a7da638b963929f22b8e8cf185
João Vitor Farias <jvmnc232@gmail.com>
2024-02-01 18:22:01 -0300

    :sparkles: feat: add ServiceItem component

--- app/(home)/_components/barbershopItem.tsx
+++ app/(home)/_components/barbershopItem.tsx
@@ -1,20 +1,30 @@
+"use client"
+
 import { Card, CardContent } from "@/app/_components/ui/card"
 import Image from "next/image"
 import { Barbershop } from "@prisma/client"
 import { Button } from "@/app/_components/ui/button";
 import { Badge } from "@/app/_components/ui/badge";
 import { StarIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
     barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+
+    const router = useRouter()
+    const handleBookingClick = () => {
+        router.push(`/barbershops/${barbershop.id}`)
+    }
+
+
     return (
         <Card className="min-w-[167px] max-w-[167px] rounded-2xl ">
             <CardContent className="px-1 py-0">
                 <div className="relative w-full h-[159px]">
-                    <div className="absolute top-3 left-3 z-50">
+                    <div className="absolute top-2.5 left-1.5 z-50">
                         <Badge variant="secondary" className=" opacity-90 flex gap-1 items-center top-2 left-2 ">
                             <StarIcon size={12} className="fill-primary text-primary" />
                             <span className="text-xs" >5,0</span>
@@ -24,13 +34,13 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
                         src={barbershop.imageUrl}
                         style={{ objectFit: 'cover' }}
                         fill
-                        className="rounded-2xl"
+                        className="rounded-2xl py-1"
                         alt={barbershop.name} />
                 </div>
                 <div className="px-2 pb-3">
                     <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap"> {barbershop.name} </h2>
                     <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap" > {barbershop.address} </p>
-                    <Button variant="secondary" className="w-full mt-3" >Reservar</Button>
+                    <Button variant="secondary" className="w-full mt-3" onClick={handleBookingClick} >Reservar</Button>
                 </div>
 
             </CardContent>
