e168b5b7fedef767931c6d06a598f8efa021168b
João Vitor Farias <jvmnc232@gmail.com>
2024-02-05 15:46:59 -0300

    :sparkles: feat: add bookings page

--- app/_components/bookingItem.tsx
+++ app/_components/bookingItem.tsx
@@ -1,26 +1,63 @@
+import { getServerSession } from "next-auth"
+import { db } from "../_lib/prisma"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Badge } from "./ui/badge"
 import { Card, CardContent } from "./ui/card"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { Booking, Prisma } from "@prisma/client"
+import { format } from "date-fns/format"
+import { ptBR } from "date-fns/locale"
+import { isFuture, isPast } from "date-fns"
 
-const BookingItem = () => {
+interface BookingItemProps {
+    booking: Prisma.BookingGetPayload<{
+        include: {
+            service: true,
+            barbershop: true,
+            user: true,
+        },
+    }>
+}
+
+// quero incluir os serviços dentro do booking, para isso
+
+const BookingItem = ({ booking }: BookingItemProps) => {
+
+    const isBookingConfirmed = isFuture(booking.date)
+
     return (
         <Card>
-            <CardContent className="p-5 flex flex-row justify-between items-center py-0 " >
-                <div className="flex flex-col gap-2 py-5 " >
-                    <Badge className="bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit">Confirmado</Badge>
-                    <h2 className="font-bold" >Corte de Cabelo</h2>
+            <CardContent className=" flex flex-row  items-center py-0 px-0 " >
+                <div className="flex flex-col gap-2 py-5 flex-[3] pl-5" >
+                    <Badge variant={isBookingConfirmed ? "default" : "secondary"} className="w-fit">{
+                        isBookingConfirmed ? "Confirmado" : "Finalizado"
+
+                    }</Badge>
+                    <h2 className="font-bold">{booking.service.name}</h2>
                     <div className="flex items-center gap-2" >
                         <Avatar className="h-6 w-6" >
-                            <AvatarImage src="https://imguol.com/c/esporte/de/2023/12/30/cristiano-ronaldo-comemora-apos-vitoria-do-al-nassr-contra-o-al-taawoun-pelo-sauditao-1703981349096_v2_450x450.jpg" />
-                            <AvatarFallback>A</AvatarFallback>
+                            <AvatarImage src={booking.barbershop.imageUrl} />
+                            <AvatarFallback> { } </AvatarFallback>
                         </Avatar>
-                        <h3 className="text-sm" >Vintage Barber</h3>
+                        <h3 className="text-sm" > {booking.barbershop.name} </h3>
                     </div>
                 </div>
-                <div className="flex flex-col items-center border-l border-solid border-secondary px-3" >
-                    <p className="text-sm" >Fevereiro</p>
-                    <p className="text-2xl">06</p>
-                    <p className="text-sm" >09:45</p>
+                <div className="flex flex-col items-center border-l border-solid border-secondary px-3 flex-1" >
+                    <p className="text-sm capitalize " > {format(
+                        booking.date, "MMMM", {
+                        locale: ptBR
+                    }
+                    )} </p>
+                    <p className="text-2xl ">  {format(
+                        booking.date, "dd", {
+                        locale: ptBR
+                    }
+                    )}</p>
+                    <p className="text-sm"> {format(
+                        booking.date, "hh:mm", {
+                        locale: ptBR
+                    }
+                    )}</p>
                 </div>
             </CardContent>
         </Card>
---
+++ app/bookings/page.tsx
@@ -0,0 +1,64 @@
+
+
+import { getServerSession } from "next-auth"
+import Header from "../_components/header"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation";
+import BookingItem from "../_components/bookingItem";
+import { db } from "../_lib/prisma";
+import { isFuture, isPast } from "date-fns";
+
+
+const BookingsPage = async () => {
+
+    // recuperar a sessão do usuário, para ver se ele está logado ou não
+    // se ele não estiver logado, será redirecionado para a home
+
+    const session = await getServerSession(authOptions);
+
+    // usar midlewares
+
+    if (!session?.user) {
+        return redirect("/")
+    }
+
+    const bookings = await db.booking.findMany({
+        where: {
+            userId: (session.user as any).id
+        },
+        include: {
+            service: true,
+            barbershop: true,
+            user: true,
+        },
+    })
+
+    const confirmedBookings = bookings.filter(booking => isFuture(booking.date))
+    const finishedBookings = bookings.filter(booking => isPast(booking.date))
+
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col px-5 py-6" >
+                <h1 className="text-xl font-bold">Agendamentos</h1>
+                <div>
+                    <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3" >CONFIRMADOS</h2>
+                    <div className="flex flex-col gap-3 mt-3    ">
+                        {confirmedBookings.map((booking) => (
+                            <BookingItem booking={booking} key={booking.id} />
+                        ))}
+                    </div>
+                    <h2 className="text-gray-400 uppercase font-bold text-sm mt-6 mb-3" >FINALIZADOS</h2>
+                    <div className="flex flex-col gap-3 mt-3    ">
+                        {finishedBookings.map((booking) => (
+                            <BookingItem booking={booking} key={booking.id} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default BookingsPage
